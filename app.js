2638b09ff15580fb5524b68206fd92c470b9219d
Nir Barzilay <nbtk123@gmail.com>
2016-10-29 13:51:02 +0300

    Running minecraft command via /time route

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
+var sys = require('sys')
+var exec = require('child_process').exec;
+
+// Somehow related to the child-process
+function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/time', function (req, res) {
+  exec("screen -S minecraft -X stuff '/time query daytime\n'", puts);
+  res.send('Time queried!');
 });
 
 app.listen(80, function () {
